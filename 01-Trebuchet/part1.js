888fc5919cc565e081bb743b83ef71efc9552d96
Casey Merkey <casey.merkey@gmail.com>
2023-12-01 12:03:43 -0500

    Day 1, part 1

---
+++ 01-Trebuchet/part1.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function parseInput() {
+
+    // Not sure why, but VSCode's debugger didn't like the relative path
+    const fileStream = fs.createReadStream(`${__dirname}/01-Trebuchet-input.txt`);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    let total = 0;
+    for await (const line of rl) {
+        const lineValue = numberFromLine(line);
+        // console.log(`Line value: ${lineValue}`);
+        total += lineValue;
+    }
+    console.log(`Total: ${total}`);
+
+}
+
+function numberFromLine(line) {
+
+    let firstDigit = null;
+    let lastDigit = null;
+    for (let i = 0; i < line.length; i++) {
+        const ch = line.charAt(i);
+        if ((ch >= '0') && (ch <= '9')) {
+            if (firstDigit == null) {
+                firstDigit = ch;
+            }
+            lastDigit = ch;
+        }
+    }
+    return parseInt(`${firstDigit}${lastDigit}`);
+}
+
+parseInput();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# advent-2023
-Advent of Code 2023
+# Advent of Code 2023
+
+https://adventofcode.com/2023
