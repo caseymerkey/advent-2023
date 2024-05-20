4d091686ccf5597a3150e24407897a9e983fb599
Casey Merkey <casey.merkey@gmail.com>
2024-05-20 16:06:32 -0400

    Part 2 working.

--- 14-Parabolic_Reflector_Dish/main.go
+++ 14-Parabolic_Reflector_Dish/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -22,35 +24,92 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	var startTime = time.Now()
-
 	var puzzle [][]byte
 	for scanner.Scan() {
 		line := scanner.Text()
 		b := []byte(line)
 		puzzle = append(puzzle, b)
 	}
-	fmt.Println(puzzle)
-	puzzle = transpose(puzzle)
-	fmt.Println(puzzle)
 
+	puzzle = rotate(puzzle, 1)
+
+	part1Copy := make([][]byte, len(puzzle))
+	for i := range puzzle {
+		part1Copy[i] = make([]byte, len(puzzle[i]))
+		copy(part1Copy[i], puzzle[i])
+	}
+
+	fmt.Println("Part 1 Input --->")
+	fmt.Println(toString(part1Copy))
+	fmt.Println()
+	part1(part1Copy)
+
+	fmt.Println("Part 2 Input --->")
+	fmt.Println(toString(rotate(puzzle, -1)))
+	fmt.Println()
+	part2(puzzle)
+}
+
+func part1(puzzle [][]byte) {
+
+	var startTime = time.Now()
 	for i, row := range puzzle {
 		puzzle[i] = shiftRow(row)
 	}
-	fmt.Println(puzzle)
+	fmt.Println(toString(puzzle))
 
-	total := 0
+	total := total(puzzle)
 
-	for x := 0; x < len(puzzle[0]); x++ {
-		for y := 0; y < len(puzzle); y++ {
-			if puzzle[x][y] == 'O' {
-				total += (y + 1)
+	executionTime := float32(time.Now().Sub(startTime).Milliseconds()) / float32(1000)
+	fmt.Printf("Part 1 Total = %d\nCompleted in %f sec\n", total, executionTime)
+}
+
+func part2(puzzle [][]byte) {
+	var startTime = time.Now()
+
+	var str string
+	var history map[string]int
+	history = make(map[string]int)
+	var indexedPuzzleStates []string
+
+	goal := 1000000000
+	for cycle := 0; cycle < goal; cycle++ {
+		for n := 0; n < 4; n++ {
+			for i, row := range puzzle {
+				puzzle[i] = shiftRow(row)
 			}
+			puzzle = rotate(puzzle, 1)
+		}
+
+		str = toString(puzzle)
+		loopStart, exists := history[str]
+		if exists {
+			// we've seen this before and will be repeating from here out
+			fmt.Printf("Found a repeat. %d repeats %d\n\n", cycle, loopStart)
+			loopLength := cycle - loopStart
+			cyclesRemaining := goal - cycle - 1
+			offset := cyclesRemaining % loopLength
+			idx := loopStart + offset
+			puzzleString := indexedPuzzleStates[idx]
+			puzzle = make([][]byte, 0)
+			for _, s := range strings.Split(puzzleString, "\n") {
+				if len(s) > 0 {
+					puzzle = append(puzzle, []byte(s))
+				}
+			}
+			break
+		} else {
+			// Add it to the history
+			history[str] = cycle
+			indexedPuzzleStates = append(indexedPuzzleStates, str)
 		}
 	}
 
+	fmt.Println(toString(rotate(puzzle, -1)))
+
+	total := total(puzzle)
 	executionTime := float32(time.Now().Sub(startTime).Milliseconds()) / float32(1000)
-	fmt.Printf("Total = %d\nCompleted in %f sec\n", total, executionTime)
+	fmt.Printf("Part 2 Total = %d\nCompleted in %f sec\n", total, executionTime)
 }
 
 func shiftRow(row []byte) []byte {
@@ -81,17 +140,52 @@ func shiftRow(row []byte) []byte {
 
 // rotate grid 90 degrees clockwise
 // "North" becomes
-func transpose(slice [][]byte) [][]byte {
-	xl := len(slice[0])
-	yl := len(slice)
-	result := make([][]byte, xl)
-	for i := range result {
-		result[i] = make([]byte, yl)
+func rotate(slice [][]byte, direction int) [][]byte {
+	if direction == 0 {
+		return slice
 	}
-	for i := 0; i < xl; i++ {
-		for j := 0; j < yl; j++ {
-			result[i][j] = slice[yl-j-1][i]
+
+	size := len(slice[0])
+
+	result := make([][]byte, size)
+	for i := range result {
+		result[i] = make([]byte, size)
+	}
+	if direction > 0 {
+		for r := 0; r < size; r++ {
+			for c := 0; c < size; c++ {
+				result[r][c] = slice[size-c-1][r]
+			}
+		}
+	} else {
+		for r := 0; r < size; r++ {
+			for c := 0; c < size; c++ {
+				result[r][c] = slice[c][size-r-1]
+			}
 		}
 	}
 	return result
+}
+
+func toString(puzzle [][]byte) string {
+
+	var buffer bytes.Buffer
+	for _, b := range puzzle {
+		buffer.Write(b)
+		buffer.WriteByte('\n')
+	}
+	return buffer.String()
+}
+
+func total(puzzle [][]byte) int {
+	total := 0
+
+	for row := 0; row < len(puzzle[0]); row++ {
+		for col := 0; col < len(puzzle); col++ {
+			if puzzle[row][col] == 'O' {
+				total += (col + 1)
+			}
+		}
+	}
+	return total
 }
